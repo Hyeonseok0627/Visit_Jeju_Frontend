5ef0e987b3899512aafe47ac4a1656bdc9ac516f
Hyeonseok0627 <stonehead0627@gmail.com>
2023-12-11 15:29:05 +0900

    변경작업(write view-업로드로 선택한 이미지 취소 버튼 코드 작성)(231211)[15:28]

--- app/src/main/java/com/example/visit_jeju_app/community/activity/CommWriteActivity.kt
+++ app/src/main/java/com/example/visit_jeju_app/community/activity/CommWriteActivity.kt
@@ -43,6 +43,9 @@ import java.util.Date
 class CommWriteActivity : AppCompatActivity() {
     lateinit var binding : ActivityCommWriteBinding
 
+    // 이미지뷰에 설정할 기본 이미지 리소스 ID
+    private val defaultImageResId = R.drawable.imageupload1
+
     // 공통 메인 레이아웃 적용 코드
     //액션버튼 토글 설정 코드
     // ActionBarDrawerToggle은 액션바에 가로줄 3개 아이콘 및 네비게이션 드로어를 제어하는 데 사용되는 클래스
@@ -211,9 +214,34 @@ class CommWriteActivity : AppCompatActivity() {
 
         }
 
+        // 업로드로 선택한 이미지 취소 버튼 코드 작성
+        // 이미지 취소 버튼에 클릭 리스너 추가
+        binding.cancel.setOnClickListener {
+            cancelImageSelection()
+        }
+
     }//onCreate
 
     // 함수 구현 ---------------------------------------------------------------------------
+
+    // 업로드로 선택한 이미지 취소 버튼 코드 작성
+    // 취소 버튼 클릭 시 호출되는 함수
+    // 이미지뷰에 이미지 로드
+    private fun loadImage(imageUri: Uri) {
+        Glide.with(this)
+            .load(imageUri)
+            .apply(RequestOptions().override(250, 200))
+            .centerCrop()
+            .into(binding.imageView)
+    }
+
+    // 업로드로 선택한 이미지 취소 버튼 코드 작성
+    private fun cancelImageSelection() {
+        // 이미지뷰를 기본 이미지로 설정
+        loadImage(Uri.parse("android.resource://" + packageName + "/" + defaultImageResId))
+        // 파일 경로 초기화
+        filePath = ""
+    }
 
     // 툴바의 검색 뷰(공통 레이아웃)
     // onCreateOptionsMenu 메서드: 액션바에 검색 기능 추가
