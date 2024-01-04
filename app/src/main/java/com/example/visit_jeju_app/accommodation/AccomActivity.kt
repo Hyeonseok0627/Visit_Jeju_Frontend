05fac5234c2a418a5efe03049c83ca1efaf48da3
Hyeonseok0627 <stonehead0627@gmail.com>
2024-01-04 12:28:11 +0900

    숙박 엑티비티 페이징처리 시작 전 세팅완료(240104)[11:38]

--- app/src/main/java/com/example/visit_jeju_app/accommodation/AccomActivity.kt
+++ app/src/main/java/com/example/visit_jeju_app/accommodation/AccomActivity.kt
@@ -59,50 +59,27 @@ class AccomActivity : AppCompatActivity() {
     lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장하는
     private val REQUEST_PERMISSION_LOCATION = 10
 
-    //액션버튼 토글
-    lateinit var toggle: ActionBarDrawerToggle
-
-    // URL link
-    private fun openWebPage(url: String) {
-        val webpage = Uri.parse(url)
-        val intent = Intent(Intent.ACTION_VIEW, webpage)
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
-    }
-
+    var accomPage : Int = 0
 
     lateinit var binding: ActivityAccomBinding
 
+    //액션버튼 토글
+    lateinit var toggle: ActionBarDrawerToggle
+
     // 서브메인에서 위치변경 없을 시, 백엔드에 데이터 요청 방지
     private var lastKnownLocation: Location? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAccomBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        handler = Handler(Looper.getMainLooper())
+        // 공통 레이아웃 시작 -------------------------------------------------------------
+        // 액션바
+        setSupportActionBar(binding.toolbar)
 
-        mLocationRequest = LocationRequest.create().apply {
-
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            interval = 30000
-            //초기화 시간 30초
-
-        }
-
-        if (checkPermissionForLocation(this)) {
-            startLocationUpdates()
-
-        }
-
-        // 추가
-        binding.pageChange.setOnClickListener {
-            val intent = Intent(this@AccomActivity, AccomRegionNmActivity::class.java)
-            startActivity(intent)
-        }
-
+        //(공통 레이아웃 코드)
         val headerView = binding.mainDrawerView.getHeaderView(0)
         val headerUserEmail = headerView.findViewById<TextView>(R.id.headerUserEmail)
         val headerLogoutBtn = headerView.findViewById<Button>(R.id.headerLogoutBtn)
@@ -119,9 +96,6 @@ class AccomActivity : AppCompatActivity() {
 
         val userEmail = intent.getStringExtra("USER_EMAIL") ?: "No Email"
         headerUserEmail.text = userEmail
-
-        // 액션바
-        setSupportActionBar(binding.toolbar)
 
         //드로워화면 액션버튼 클릭 시 드로워 화면 나오게 하기
         toggle =
@@ -197,9 +171,73 @@ class AccomActivity : AppCompatActivity() {
                 else -> false
             }
         }
+        // 공통 레이아웃 끝 -------------------------------------------------------------
 
+        handler = Handler(Looper.getMainLooper())
+
+        mLocationRequest = LocationRequest.create().apply {
+
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 30000
+            //초기화 시간 30초
+
+        }
+
+        if (checkPermissionForLocation(this)) {
+            startLocationUpdates()
+
+        }
+
+
+        binding.pageChange.setOnClickListener {
+            val intent = Intent(this@AccomActivity, AccomRegionNmActivity::class.java)
+            startActivity(intent)
+        }
     }//oncreate
 
+    // 함수 구현 ---------------------------------------------------------------------------
+
+    // Bottom Navigation link(공통 레이아웃 코드)
+    private fun openWebPage(url: String) {
+        val webpage = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    // menu 기능
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu,menu)
+
+        // 검색 뷰에, 이벤트 추가하기.
+        val menuItem = menu?.findItem(R.id.menu_toolbar_search)
+        // menuItem 의 형을 SearchView 타입으로 변환, 형변환
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //검색어가 변경시 마다, 실행될 로직을 추가.
+                Log.d("kmk","텍스트 변경시 마다 호출 : ${newText} ")
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // 검색어가 제출이 되었을 경우, 연결할 로직.
+                // 사용자 디비, 검색을하고, 그 결과 뷰를 출력하는 형태.
+                Toast.makeText(this@AccomActivity,"검색어가 전송됨 : ${query}", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
 
     private fun startLocationUpdates() {
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
@@ -233,8 +271,11 @@ class AccomActivity : AppCompatActivity() {
         if (lastKnownLocation == null || isLocationChanged(location, lastKnownLocation!!)) {
             mLastLocation = location
             lastKnownLocation = location
-            val coords = "${mLastLocation.longitude},${mLastLocation.latitude}"
-            getAccomListWithinRadius(coords)
+            val pref = getSharedPreferences("latlnt", MODE_PRIVATE)
+            val lat: Double? = pref.getString("lat", null)?.toDoubleOrNull()
+            val lnt: Double? = pref.getString("lnt", null)?.toDoubleOrNull()
+
+            getAccomListWithinRadius(lat, lnt, 7.0, accomPage)
         }
     }
 
@@ -243,52 +284,20 @@ class AccomActivity : AppCompatActivity() {
         return newLocation.latitude != lastLocation.latitude || newLocation.longitude != lastLocation.longitude
     }
 
-    private fun haversineDistance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
-        // 반경 필터링
-        val R = 6371.0 // 지구의 반지름 (단위: km)
-
-        val dLat = Math.toRadians(lat2 - lat1)
-        val dLon = Math.toRadians(lon2 - lon1)
-
-        val a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-                Math.sin(dLon / 2) * Math.sin(dLon / 2)
-        val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-
-        return R * c
-    }
-
-    private fun getAccomListWithinRadius(coords: String) {
+    private fun getAccomListWithinRadius(lat: Double?, lnt: Double?, radius : Double, accomPage : Int) {
 
         val networkService = (applicationContext as MyApplication).networkService
-        val accomListCall = networkService.GetAccomList()
+        val accomListCall = networkService.getAccomGPS(lat, lnt, radius , accomPage)
 
-        accomListCall.enqueue(object : Callback<List<AccomList>> {
+        accomListCall.enqueue(object : Callback<MutableList<AccomList>> {
             override fun onResponse(
-                call: Call<List<AccomList>>,
-                accomponse: Response<List<AccomList>>
+                call: Call<MutableList<AccomList>>,
+                response: Response<MutableList<AccomList>>
 
             ) {
-                val accomList = accomponse.body()
+                val accomList = response.body()
 
-                Log.d("ljs","accomModel 값 : ${accomList}")
-
-                val centerLatitude = mLastLocation.latitude
-                val centerLongitude = mLastLocation.longitude
-                val radius = 5.0 // 5km 반경
-
-
-                val accomistSpotsWithinRadius = accomList?.mapNotNull { spot ->
-                    val distance = haversineDistance(
-                        centerLatitude, centerLongitude,
-                        spot.itemsLatitude, spot.itemsLongitude
-                    )
-                    if (distance <= radius) {
-                        spot // 관광지 데이터 객체 자체를 반환
-                    } else {
-                        null
-                    }
-                }
+                Log.d("lhs","accomModel 값 : ${accomList}")
 
                 val currentTime = System.currentTimeMillis()
 
@@ -304,7 +313,7 @@ class AccomActivity : AppCompatActivity() {
                 binding.recyclerView.layoutManager = layoutManager
 
                 binding.recyclerView.adapter =
-                    AccomAdapter(this@AccomActivity,accomistSpotsWithinRadius)
+                    AccomAdapter(this@AccomActivity,accomList)
 
 
                 binding.recyclerView.addItemDecoration(
@@ -314,8 +323,8 @@ class AccomActivity : AppCompatActivity() {
             }
 
 
-            override fun onFailure(call: Call<List<AccomList>>, t: Throwable) {
-                Log.d("lsy", "fail")
+            override fun onFailure(call: Call<MutableList<AccomList>>, t: Throwable) {
+                Log.d("lhs", "fail")
                 call.cancel()
             }
         })
@@ -356,39 +365,4 @@ class AccomActivity : AppCompatActivity() {
         super.onDestroy()
         handler.removeCallbacksAndMessages(null) // 핸들러 메시지 제거
     }
-
-
-    // menu 기능
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)) {
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar_menu,menu)
-
-        // 검색 뷰에, 이벤트 추가하기.
-        val menuItem = menu?.findItem(R.id.menu_toolbar_search)
-        // menuItem 의 형을 SearchView 타입으로 변환, 형변환
-        val searchView = menuItem?.actionView as SearchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(newText: String?): Boolean {
-                //검색어가 변경시 마다, 실행될 로직을 추가.
-                Log.d("kmk","텍스트 변경시 마다 호출 : ${newText} ")
-                return true
-            }
-
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                // 검색어가 제출이 되었을 경우, 연결할 로직.
-                // 사용자 디비, 검색을하고, 그 결과 뷰를 출력하는 형태.
-                Toast.makeText(this@AccomActivity,"검색어가 전송됨 : ${query}", Toast.LENGTH_SHORT).show()
-                return true
-            }
-        })
-
-        return super.onCreateOptionsMenu(menu)
-    }
-
 }
